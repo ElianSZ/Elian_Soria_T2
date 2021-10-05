c6dea6e7934397efe122f8c6bce8adcb44bcf651
Elián Soria Zamora <eliansoria7@gmail.com>
2021-10-05 14:29:15 +0200

    T2

--- Elian_Tema_2/Assets/Scripts/MyFirstScript.cs
+++ Elian_Tema_2/Assets/Scripts/MyFirstScript.cs
@@ -4,22 +4,56 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    /*
     public int playerAge = 35;
     private float playerSpeed = 5.25f;
+    [SerializeField] private bool gameOver = false;
+    */
+
     public string playerName = "Elian";
-    [SerializeField]
-    private bool gameOver = false;
+    public string enemyName = "Elian ";
+
+    [SerializeField] private int x = 5;
+    [SerializeField] private int y = 3;
+    [SerializeField] private int z = 3;
+
+    [SerializeField] private bool isRaining;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       /*
+       Debug.Log(playerAge);
+       Debug.Log(message: $"¡Hola {playerName}!");
+       */
+
+        /*
+        Debug.Log(message: $"Suma: {x} + {y} = {x + y}");
+        Debug.Log(message: string.Format("Resta: {0} - {1} = {2}", x, y, x - y));
+        Debug.Log(message: string.Format("Producto: {0} * {1} = {2}", x, y, x * y));
+        Debug.Log(message: string.Format("Division: {0} / {1} = {2}", x, y, x / y));
+       */
+
+        if (playerName == enemyName)
+        {
+            Debug.Log(message: "Player y enemigo se llaman igual");
+        }
+
+        if (playerName != enemyName)
+        {
+            Debug.Log(message: "Player y enemigo tienen distinto nombre");
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRaining)
+        {
+            Debug.Log(message: "Llevate un paraguas");
+        }
     }
 }
--- Elian_Tema_2/Assets/Scripts/Vehicle.cs
+++ Elian_Tema_2/Assets/Scripts/Vehicle.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    // La variable sound guarda el sonido que hace el vehículo
+    [SerializeField] private string sound = "¡Brum, brum!";
+
+    // La variable name guarda a que vehiculo se refiere y la varible numberWheels guarda el numero de ruedas que tiene el vehiculo
+    [SerializeField] private string name = "";
+    [SerializeField] private int numberWheels = 4;
+
+    // La variable hasSiren guarda la sirena
+    public bool hasSiren;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(message: $"{name} tiene {numberWheels} ruedas y hace {sound}");
+
+        // El operador de decision comprueba si el vehiculo tiene sirena
+        if (hasSiren == true)
+        {
+            Debug.Log(message: $"{name} tiene sirena");
+        }
+
     }
 
     // Update is called once per frame
